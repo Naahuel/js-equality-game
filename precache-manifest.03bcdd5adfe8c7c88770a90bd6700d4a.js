2535451c6691ebd8cd46d8880e702691d56248dd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-31 10:23:26 +0000

    Deploy slikts/js-equality-game to github.com/slikts/js-equality-game.git:gh-pages

---
+++ precache-manifest.03bcdd5adfe8c7c88770a90bd6700d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aba8cf3e82498c682117",
+    "url": "/static/css/main.1a7c01e8.chunk.css"
+  },
+  {
+    "revision": "aba8cf3e82498c682117",
+    "url": "/static/js/main.aba8cf3e.chunk.js"
+  },
+  {
+    "revision": "ccf394b25073f5133356",
+    "url": "/static/js/1.ccf394b2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b14f4fada2e9b1484d355e69f31677f9",
+    "url": "/index.html"
+  }
+];
